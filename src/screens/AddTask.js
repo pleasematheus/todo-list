fcdceadd9ab5745fc72fd7dc22466534e75349f7
pleasematheus <matheus4k@gmail.com>
2023-08-31 18:57:24 -0400

    Upgraded modal

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react"
-import { Modal, View, StyleSheet, TouchableWithoutFeedback, Text } from 'react-native'
+import {
+	Modal,
+	View,
+	StyleSheet,
+	TouchableWithoutFeedback,
+	Text,
+	TouchableOpacity,
+	TextInput,
+	Button
+} from 'react-native'
 import commonStyles from "../commonStyles"
 
+const initialState = {desc: ''}
+
 export default class AddTask extends Component {
+
+	state = {
+		...initialState
+	}
+
 	render() {
 		return (
 			<Modal
@@ -19,6 +35,20 @@ export default class AddTask extends Component {
 
 				<View style={styles.container}>
 					<Text style={styles.header}>Nova Tarefa</Text>
+					<TextInput
+						style={styles.input}
+						placeholder="Informe a descrição"
+						onChangeText={desc => this.setState({ desc })}
+						value={this.state.desc}
+					/>
+					<View style={styles.buttons}>
+						<TouchableOpacity onPress={this.props.onCancel}>
+							<Text style={styles.button}>Cancelar</Text>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={undefined}>
+							<Text style={styles.button}>Salvar</Text>
+						</TouchableOpacity>
+					</View>
 				</View>
 
 				<TouchableWithoutFeedback onPress={this.props.onCancel}>
@@ -37,7 +67,6 @@ const styles = StyleSheet.create({
 		backgroundColor: "rgba(0, 0, 0, 0.7)"
 	},
 	container: {
-		flex: 1,
 		backgroundColor: '#FFF'
 	},
 	header: {
@@ -47,5 +76,23 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		padding: 15,
 		fontSize: 18
+	},
+	input: {
+		fontFamily: commonStyles.fontFamily,
+		height: 40,
+		margin: 15,
+		backgroundColor: '#FFF',
+		borderWidth: 1,
+		borderColor: '#e3e3e3',
+		borderRadius: 6
+	},
+	buttons: {
+		flexDirection: 'row',
+		justifyContent: 'flex-end',
+	},
+	button: {
+		margin: 20,
+		marginRight: 30,
+		color: commonStyles.colors.today
 	}
 })
