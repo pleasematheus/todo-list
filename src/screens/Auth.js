0e2f7ba3b996cb291014c6180722a76ae6e36998
pleasematheus <matheus4k@gmail.com>
2024-03-19 03:56:20 -0400

    Added signup alerts

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -8,25 +8,46 @@ import {
 	Alert
 } from 'react-native'
 
+import axios from 'axios'
+
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
 import AuthInput from '../components/AuthInput'
 
+import { server, showError, showSucess } from '../common'
+
+const initialState = {
+	name: '',
+	email: '',
+	password: '',
+	confirmPassword: '',
+	stageNew: false
+}
 export default class Auth extends Component {
 
-	state = {
-		name: '',
-		email: '',
-		password: '',
-		confirmPassword: '',
-		stageNew: false
-	}
+	state = { ...initialState }
 
 	signinOrSignup = () => {
 		if (this.state.stageNew) {
-			Alert.alert('Sucesso!', 'Criar conta')
+			this.signup()
 		} else {
 			Alert.alert('Sucesso', 'Logar')
+		}
+	}
+
+	signup = async () => {
+		try {
+			await axios.post(`${server}/signup`, {
+				name: this.state.name,
+				email: this.state.email,
+				password: this.state.password,
+				confirmPassword: this.state.confirmPassword
+			})
+
+			showSucess('Usuário cadastrado!')
+			this.setState({...initialState})
+		} catch (e) {
+			showError(e)
 		}
 	}
 
